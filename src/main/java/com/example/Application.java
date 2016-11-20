bf1982dbcd8e109b9b76458a9472edb81b7f0e00
dong <120679341@qq.com>
2016-11-20 17:12:01 +0800

    renyd

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,5 +1,8 @@
 package com.example;
 
+import com.example.aop.GlobalExceptionHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,7 +12,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
     public static void main(String[] args) {
+        logger.info("system start. args:{}", args);
         SpringApplication.run(Application.class, args);
     }
 
